2cf1fa9d2f47040c37907fc9ae2380a7211953b6
SoDRUNK <antoine.lefevre@supinternet.fr>
2019-01-28 15:56:56 +0100

    add navbar and search function

---
+++ src/components/Favo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container centered">
+
+    <h1>Liste de vos favoris</h1>
+    <hr>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Favo',
+  components: {
+  },
+  data() {
+    return {
+      collaborateurs: null,
+      collaborateur: null
+    }
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
